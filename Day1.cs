038c60d10c92e9882af284faa680f4ee2adfa21c
Adam Mahood <adam.m.mahood@gmail.com>
2018-12-02 13:41:23 -0800

    Infra and Day 1

---
+++ Day1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AoC2018
+{
+    public class Day1
+    {
+        public static void TestDay1()
+        {
+            Console.WriteLine("-------------------DAY 1-------------------");
+
+            StreamReader sr = new StreamReader("Day1Input.txt");
+            string line;
+            
+            int frequencyDrift = 0;
+            HashSet<int> frequencyTracker = new HashSet<int>();
+            bool repeatFound = false;
+            frequencyTracker.Add(0);
+
+            //Part 1 - For each line, determine if positive or negative and adjust running tally
+            //Part 2 - Try HashSet usage for Part 2
+            while (!repeatFound)
+            {
+                line = sr.ReadLine();
+                if (line==null){
+                    sr.DiscardBufferedData();
+                    sr.BaseStream.Seek(0, System.IO.SeekOrigin.Begin); 
+                    line = sr.ReadLine();
+                }
+                char sign = line[0];
+                int numberToProcess = Int32.Parse(line.Substring(1));
+                switch (sign)
+                {
+                    case '+':
+                        frequencyDrift += numberToProcess;
+                        if (frequencyTracker.Contains(frequencyDrift))
+                        {
+                            repeatFound = true;
+                        }
+                        else
+                        {
+                            frequencyTracker.Add(frequencyDrift);
+                        }
+                        break;
+                    case '-':
+                        frequencyDrift -= numberToProcess;
+                        if (frequencyTracker.Contains(frequencyDrift))
+                        {
+                            repeatFound = true;
+                        }
+                        else
+                        {
+                            frequencyTracker.Add(frequencyDrift);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Didn't get valid sign :(");
+                        break;
+                }
+                if (repeatFound){Console.WriteLine("Repeat Found - "+frequencyDrift);}
+            }
+
+            //Console.WriteLine("Final frequency drift for calibration = "+frequencyDrift);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AoC2018
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Welcome to Advent of Code 2017! Please pass in which Day you would like to evaluate as a command line argument!");
+            Console.WriteLine();
+
+            int dayChallenge = Int32.Parse(args[0]);
+            if (dayChallenge <1 || dayChallenge > 25)
+            {
+                Console.WriteLine("Please pass in a valid day challenge between 1 and 25 :(");
+                Console.WriteLine();
+                return;
+            }
+
+            switch (dayChallenge)
+            {
+                case 1:
+                    Day1.TestDay1();
+                    break;
+                default:
+                    break; 
+            }
+
+            return;
+        }
+    }
+}
